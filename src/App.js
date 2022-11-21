01f8c51c6d78782076a0ab3ac3460ede5d06f41d
noorong <100012522+noorong@users.noreply.github.com>
2022-11-21 21:40:51 +0900

    section 4 11.21

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses item={expenses} />
       {/*브라우저에게 ExpenseItem이라는 컴포넌트를 쓰고싶다고 전달*/}
     </div>
